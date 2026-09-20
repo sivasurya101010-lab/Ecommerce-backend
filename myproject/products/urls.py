d7e1807806c31635632e0f7df94bcd062178cf57
surya <sivasurya101010@gmail.com>
2026-09-20 12:46:02 +0530

    Fix product URL patterns

--- myproject/products/urls.py
+++ myproject/products/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 
 urlpatterns=[
         path('categories/',CategoryView.as_view()),
-        path('edit/[int:pk](int:pk)/',ProductEditView.as_view()),
+        path('edit/<int:pk>/',ProductEditView.as_view()),
         path('',ProductListView.as_view()),
-        path('[int:pk](int:pk)/',ProductDetailView.as_view()),
+        path('<int:pk>/',ProductDetailView.as_view()),
 
 ]
